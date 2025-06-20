93cf312f3accc104a9d6a6774e126ffb4f0f865b
sanAndroid <jocampos@outlook.de>
2025-06-20 19:59:55 +0545

    :construction: improve layout of settings panel

--- src/main/kotlin/com/github/sanandroid/jnforce/settings/JnForceComponent.kt
+++ src/main/kotlin/com/github/sanandroid/jnforce/settings/JnForceComponent.kt
@@ -4,6 +4,7 @@ import com.github.sanandroid.jnforce.services.SalesforceService
 import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.ui.Messages
+import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPasswordField
@@ -11,9 +12,13 @@ import com.intellij.ui.components.JBRadioButton
 import com.intellij.ui.components.JBTextArea
 import com.intellij.ui.components.JBTextField
 import org.jetbrains.annotations.NotNull
+import java.awt.Component
+import java.awt.Dimension
 import java.awt.GridLayout
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
+import javax.swing.BorderFactory
+import javax.swing.Box
 import javax.swing.BoxLayout
 import javax.swing.ButtonGroup
 import javax.swing.JButton
@@ -49,16 +54,19 @@ const val USE_CLASS_FILTERS = "useClassFilters"
 class JnForceComponent {
 
     private lateinit var panel: JPanel
+    private lateinit var filtersPanel: JPanel
 
     private val myUsernameText = JBTextField().apply {
         name = USERNAME
     }
 
     private val myClientIdText = JBTextField().apply {
+        text = "Client ID"
         name = CLIENT_ID
     }
 
     private val myBaseUrlText = JBTextField().apply {
+        text = "Base Url"
         name = BASE_URL
     }
     private val myPackageNameText = JBTextField().apply {
@@ -111,8 +119,11 @@ class JnForceComponent {
         name = CLIENT_SECRET
     }
 
-    private val myTestConnection = JButton().apply {
+    private val myTestConnection = JButton("Test Connection").apply {
         name = TEST_CONNECTION
+        putClientProperty("JButton.buttonType", "default")
+        setForeground(JBColor.RED)
+        text = "Test Connection"
         action = TestConnectionAction()
     }
 
@@ -140,66 +151,118 @@ class JnForceComponent {
     }
 
     internal fun buildPanel() {
-
         panel = JPanel().apply {
-            layout = GridLayout(0, 1)
-            add(JBLabel("ClientId"))
-            add(myClientIdText)
-            add(JBLabel("ClientSecret"))
-            add(myClientSecretText)
-            add(JBLabel("Package name"))
-            add(myPackageNameText)
-            add(JBLabel("Class path"))
-            add(myClassPathText)
-            add(JBLabel("Base url"))
-            add(myBaseUrlText)
-            add(myTestConnection)
-            JPanel().apply {
-                BoxLayout(this, BoxLayout.X_AXIS)
-                add(JBLabel("Use Class List"))
-                add(myUseClassListButton)
-                add(JBLabel("Use Class Filters"))
-                add(myUseClassFiltersButton)
-                add(myClassListTextField)
-                JPanel().apply {
-                    BoxLayout(this, BoxLayout.X_AXIS)
-                    JPanel().apply {
-                        BoxLayout(this, BoxLayout.Y_AXIS)
-                        add(myUseClassListButton).apply {
-                            add(myFilterCreatable)
-                            add(myFilterCustom)
-                            add(myFilterDeletable)
-                            add(myFilterLayoutable)
-                            add(myFilterMergeable)
-                        }
-                        JPanel().apply {
-                            BoxLayout(this, BoxLayout.Y_AXIS)
-                            add(myFilterReplicateable)
-                            add(myFilterRetrievable)
-                            add(myFilterSearchable)
-                            add(myFilterUpdateable)
-                        }
-                    }
-                }
-                filterOrListVisible()
+            layout = BoxLayout(this, BoxLayout.Y_AXIS)
+            alignmentX = Component.LEFT_ALIGNMENT
+            alignmentY = Component.TOP_ALIGNMENT
+            val mainSettingsPanel = JPanel().apply {
+                layout = GridLayout(0, 1, 10, 10)
+                // layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                add(JBLabel("ClientId"))
+                add(myClientIdText)
+                add(JBLabel("ClientSecret"))
+                add(myClientSecretText)
+                add(JBLabel("Package name"))
+                add(myPackageNameText)
+                add(JBLabel("Class path"))
+                add(myClassPathText)
+                add(JBLabel("Base url"))
+                add(myBaseUrlText)
+                add(JBLabel("Test Connection"))
+                add(myTestConnection)
             }
+            add(mainSettingsPanel)
+            add(Box.createVerticalStrut(20));
+            val lowerPanel = JPanel().apply {
+                border = BorderFactory.createLineBorder(JBColor.GRAY, 1)
+                layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                add(Box.createVerticalStrut(20));
+                val radioPanel = JPanel().apply {
+                    layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                    alignmentX = Component.LEFT_ALIGNMENT
+                    alignmentY = Component.TOP_ALIGNMENT
+                    size = Dimension(500, 100);
+                    // layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                    add(Box.createHorizontalStrut(10))
+                    val classList = JPanel().apply {
+                        layout = BoxLayout(this, BoxLayout.X_AXIS)
+                        alignmentX = Component.LEFT_ALIGNMENT
+                        alignmentY = Component.TOP_ALIGNMENT
+                        add(JBLabel("Use Class List"))
+                        add(Box.createHorizontalStrut(20))
+                        add(myUseClassListButton)
+                    }
+                    add(classList)
+                    val filtersButton = JPanel().apply {
+                        layout = BoxLayout(this, BoxLayout.X_AXIS)
+                        alignmentX = Component.LEFT_ALIGNMENT
+                        alignmentY = Component.TOP_ALIGNMENT
+                        add(
+                            JBLabel("Use Class Filters").apply {
+                                alignmentX = Component.LEFT_ALIGNMENT
+                                alignmentY = Component.TOP_ALIGNMENT
+                            })
+                        add(Box.createHorizontalStrut(20))
+                        add(myUseClassFiltersButton.apply {
+                            alignmentX = Component.LEFT_ALIGNMENT
+                            alignmentY = Component.TOP_ALIGNMENT
+                        })
+                    }
+                    add(filtersButton)
+                }
+                add(radioPanel)
+                add(Box.createVerticalStrut(20));
+                filtersPanel = JPanel().apply {
+                    layout = BoxLayout(this, BoxLayout.X_AXIS)
+                    layout = BoxLayout(this, BoxLayout.X_AXIS)
+                    alignmentX = Component.LEFT_ALIGNMENT
+                    val leftFilterPanel = JPanel().apply {
+                        alignmentX = Component.LEFT_ALIGNMENT
+                        layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                        add(Box.createVerticalStrut(20));
+                        add(Box.createHorizontalStrut(20));
+                        add(myClassListTextField).apply {
+                            add(Box.createVerticalStrut(20));
+                            add(Box.createHorizontalStrut(20));
+                        }
+                        add(myFilterCreatable)
+                        add(myFilterCustom)
+                        add(myFilterDeletable)
+                        add(myFilterLayoutable)
+                        add(myFilterMergeable)
+                        add(Box.createVerticalStrut(20));
+                        add(Box.createHorizontalStrut(20));
+                    }
+                    val rightFilterPanel = JPanel().apply {
+                        alignmentX = Component.LEFT_ALIGNMENT
+                        layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                        add(Box.createVerticalStrut(20));
+                        add(Box.createHorizontalStrut(20));
+                        add(myFilterReplicateable)
+                        add(myFilterRetrievable)
+                        add(myFilterSearchable)
+                        add(myFilterUpdateable)
+                        add(Box.createVerticalStrut(20));
+                        add(Box.createHorizontalStrut(20));
+                    }
+                    add(leftFilterPanel)
+                    add(rightFilterPanel)
+                }
+                add(filtersPanel)
+            }
+            add(lowerPanel)
         }
+        filterOrListVisible()
     }
 
     private fun filterOrListVisible() {
         if (myUseClassListButton.isSelected) {
+            filtersPanel.isVisible = false
             myClassListTextField.isVisible = true
         }
         if (myUseClassFiltersButton.isSelected) {
-            myFilterCreatable.isVisible = true
-            myFilterCustom.isVisible = true
-            myFilterDeletable.isVisible = true
-            myFilterLayoutable.isVisible = true
-            myFilterMergeable.isVisible = true
-            myFilterReplicateable.isVisible = true
-            myFilterRetrievable.isVisible = true
-            myFilterSearchable.isVisible = true
-            myFilterUpdateable.isVisible = true
+            myClassListTextField.isVisible = false
+            filtersPanel.isVisible = true
         }
     }
 
@@ -308,17 +371,22 @@ class JnForceComponent {
     inner class TestConnectionAction : AbstractAction() {
         override fun actionPerformed(@NotNull e: ActionEvent): Unit = with(panel) {
             val project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(this))
-                ?: throw Exception("Could not obtain project, aborting action TestConnectionAction")
-            val success = SalesforceService.instance(project).testConnection()
-            if (success) {
-                Messages.showInfoMessage(
-                    project,
-                    "Successfully connected to Salesforce.",
-                    "Connection Successful"
-                )
-            } else {
+
+            project?.let {
+                val success = SalesforceService.instance(project).testConnection()
+                if (success) {
+                    Messages.showInfoMessage(
+                        project, "Successfully connected to Salesforce.", "Connection Successful"
+                    )
+                } else {
+                    Messages.showErrorDialog(
+                        project,
+                        "Authentication failed. Please check your credentials and try again.",
+                        "Connection Failed"
+                    )
+                }
+
                 Messages.showErrorDialog(
-                    project,
                     "Authentication failed. Please check your credentials and try again.",
                     "Connection Failed"
                 )
